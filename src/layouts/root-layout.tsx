2454b84c56696343cd8255d48a3cb7532b0e8939
Atom <chanidapa.atom@gmail.com>
2026-09-22 17:09:54 +0700

    lab15

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -8,7 +8,6 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { currentStudent } from "@/lib/mock-data"; // ← เพิ่มบรรทัดนี้
 
 export default function RootLayout() {
   return (
